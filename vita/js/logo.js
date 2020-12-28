4c61c342c8885c02e838fb7de44d3f4d7ae2b412
DIAMONDFPQ <76275844+DIAMONDFPQ@users.noreply.github.com>
2020-12-28 21:21:18 +0800

    Add files via upload

---
+++ vita/js/logo.js
@@ -0,0 +1,10 @@
+document.write('	<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"	');
+document.write('	        codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0"	');
+document.write('	        width="1000" height="100" id="LOGO1" align="">	');
+document.write('	    <param name=movie value="LOGO1.swf">	');
+document.write('	    <param name=quality value=high>	');
+document.write('	    <embed src="LOGO1.swf" quality=high  width="1000" height="100" 	');
+document.write('	           name="LOGO1" align=""	');
+document.write('	           type="application/x-shockwave-flash" 	');
+document.write('	           pluginspage="http://www.macromedia.com/go/getflashplayer">	');
+document.write('	</object>	');
